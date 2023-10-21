0f8ed09861655b765c15d6c74505e0e08033d307
lynnnijboer <lynnnijboer12@gmail.com>
2023-10-21 14:36:46 +0200

    added user statistics option

--- existing_users.py
+++ existing_users.py
@@ -1,11 +1,9 @@
 def print_existing_users(existing_users):
     if existing_users:
         for index, user in enumerate(existing_users):
-            print(f"[User {index + 1}]")
+            print(f"[User {index + 1}: ", end='')
             for user_key, user_value in user.items():
-                print(f"Name: {user_value['user_name']}")
-                print(f"Income: {user_value['user_income']} euro")
-            print("")
+                print(f"{user_value['user_name']}]")
     else:
         print("There are no users made yet!")
     return existing_users
