400446b1e2f2a937bc30ca2f3cc8a24865a021e2
lynnnijboer <lynnnijboer12@gmail.com>
2023-10-23 01:10:44 +0200

    added balance calculation

--- balance_calculation.py
+++ balance_calculation.py
@@ -1,2 +1,15 @@
-def balance_calculation():
-    print("I calculate balance")
+def balance_calculation(current_user, option, balance):
+    for key, values in current_user.items():
+        user_income = int(values['user_income'])
+        amount = int(input("Please enter a new income: "))
+        description = input("What is the description? ")
+        if option == 1:
+            user_income += amount
+            print(f"{amount} euro has been added to balance.")
+        elif option == 2:
+            user_income -= amount
+            print(f"{amount} euro has been subtracted from balance.")
+        values['user_income'] = user_income
+        income_entry = (amount, description)
+        balance.append(income_entry)
+    return user_income, balance
--- main.py
+++ main.py
@@ -6,15 +6,18 @@ INPUT_ERROR_MSG = "Incorrect input. Please try again.\n"
 FIRST_MENU_TEXT = '''
 Please select an option:
 1. New User 
-2. Existing User
+2. Choose user
 3. Show user statistics
+4. Add income or expense
 '''
 
 def first_menu():
 # new user - ask for income, user info
 # existing user - menu 2 - select user account - menu 3 - browse respective data
     users = None
+    current_user = None
     existing_users = []
+    balance = []
     while True:
         try:
             print(FIRST_MENU_TEXT)
@@ -26,18 +29,45 @@ def first_menu():
                 
             elif menu_choice == 2:
                 print_existing_users(existing_users)
-                option = int(input(f"What user do you want to see? Please enter number 1 - {len(existing_users)}: "))
-                for index, user in enumerate(existing_users):
-                    if option == index + 1:
-                        current_user = user
-                        for items in user.values():
-                            print(f"User {items['user_name']} has been set.")
+                if existing_users:
+                    option = int(input(f"What user would you like to see? Please enter number 1/{len(existing_users)}: "))
+                    for index, user in enumerate(existing_users):
+                        if option == index + 1:
+                            current_user = user
+                            for items in user.values():
+                                print(f"User {items['user_name']} has been set.")
                 continue
+
             elif menu_choice == 3:
                 if current_user:
+                    print("\n[User statistics]")
                     for key, values in current_user.items():
                         print(f"Name: {values['user_name']}")
-                        print(f"Income: {values['user_income']}")
+                        print(f"Income: {values['user_income']} euro")
+                    if balance:
+                        print("\n[User incomes and expenses]")
+                        for items in balance:
+                            print(f"{items[0]} euro ({items[1]})")
+                    continue
+                else:
+                    print("No user has been selected.")
+                    continue
+            
+            elif menu_choice == 4:
+                if current_user:
+                    print("1. Add income")
+                    print("2. Add expense")
+                    while True:
+                        option = int(input("Please choose 1 or 2: "))
+                        if option == 1:
+                            user_income, incomes = balance_calculation(current_user, option, balance)
+                            break
+                        elif option == 2:
+                            user_income, expenses = balance_calculation(current_user, option, balance)
+                            break
+                        else:
+                            print(INPUT_ERROR_MSG)
+                            continue
                     continue
                 else:
                     print("No user has been selected.")
